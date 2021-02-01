b84222b36d5c035af674cc62aa0e19c64dc45679
mchadds <cdundassmith@gmail.com>
2021-02-01 14:14:22 -0700

    css styling applied

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,38 @@
-import React from "react"
-import Header from "../components/header"
+import React from "react";
+import Header from "../components/header";
+import Container from "../components/container";
+import styles from "./about.module.css"
+
+const User = props => (
+    <div className={styles.user}>
+      <img src={props.avatar} className={styles.avatar} alt="" />
+      <div className={styles.description}>
+        <h2 className={styles.username}>{props.username}</h2>
+        <p className={styles.excerpt}>{props.excerpt}</p>
+      </div>
+    </div>
+  )
 
 const About = () => {
   return (
-    <div style={{ color: `teal` }}>
+   // <div style={{ color: `teal` }}>
+   <Container>
       <Header headerText="About Gatsby" />
-      <Header headerText="is pretty cool" />
+      {/* <Header headerText="is pretty cool" /> */}
       <p>Such wow. Very React.</p>
-    </div>
+      <User
+        username="Maria Randall"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
+        excerpt="I'm Maria Randall. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Daniela Dewitt"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
+        excerpt="I'm Daniela Dewitt. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+   </Container>
+      
+    //</div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
 
 const Home = () => {
   return (
